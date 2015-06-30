4dfbfa0490abc0cc5bcda3fbb603daea40a2dae6
Brendan Burns <bburns@google.com>
2015-06-30 18:48:31 +0000

    new stuff.

--- www/script.js
+++ www/script.js
@@ -2,6 +2,14 @@ var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
     $scope.data = {
-	"selected": 0
+	"selected": 0,
+        "clusters": [
+            {
+		"name": "Create a new GKE Cluster"
+            },
+	    {
+		"name": "Manual Cluster"
+	    }
+	]
     };
 }]);
