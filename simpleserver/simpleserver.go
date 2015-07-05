26f83bbbf1c3b4370d46aeaf10178f3afc12c728
Brendan Burns <bburns@google.com>
2015-07-05 05:12:12 +0000

    Update work for google API

--- simpleserver/simpleserver.go
+++ simpleserver/simpleserver.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+        "crypto/rand"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
 	"example"
-	"fmt"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/golang/glog"
+	container "github.com/google/google-api-go-client/container/v1"
 )
 
 var (
@@ -21,7 +27,9 @@ var (
 	clientSecretFile = flag.String("secret-file", "clientsecret.dat",
 		"Name of a file containing just the project's OAuth 2.0 Client Secret from https://developers.google.com/console")
 
-	client *http.Client
+	client  *http.Client
+	service *container.Service
+	selectedCluster *container.Cluster
 )
 
 func loadFileOrString(val, file string) string {
@@ -36,54 +44,172 @@ func loadFileOrString(val, file string) string {
 }
 
 func tokenHandler(res http.ResponseWriter, req *http.Request) {
-     code := req.URL.Query()["code"]
-     if len(code) == 0 {
-     	res.WriteHeader(500)
-	res.Write([]byte("Missing expected parameter"))
-	return
+	code := req.URL.Query()["code"]
+	if len(code) == 0 {
+		res.WriteHeader(500)
+		res.Write([]byte("Missing expected parameter"))
+		return
 	}
 
-     id := loadFileOrString(*clientID, *clientIDFile)
-     secret := loadFileOrString(*clientSecret, *clientSecretFile)
+	id := loadFileOrString(*clientID, *clientIDFile)
+	secret := loadFileOrString(*clientSecret, *clientSecretFile)
 	config, ctx := example.NewClientConfigAndContext(id, secret)
 
-     _, port, err := net.SplitHostPort(req.Host)
-     if err != nil {
-     	res.WriteHeader(500)
-	response := fmt.Sprintf("Error: %v", err)
-	res.Write([]byte(response))
-	return
+	_, port, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		res.WriteHeader(500)
+		response := fmt.Sprintf("Error: %v", err)
+		res.Write([]byte(response))
+		return
 	}
 
-     // This is the URL to return to
-     config.RedirectURL = fmt.Sprintf("http://localhost:%s/token", port)
+	// This is the URL to return to
+	config.RedirectURL = fmt.Sprintf("http://localhost:%s/token", port)
 
 	client = example.NewOAuthClient(ctx, config, code[0])
-
 	glog.Infof("Created client: %v", client)
-	
+	containerService, err := container.New(client)
+	if err != nil {
+		res.WriteHeader(500)
+		response := fmt.Sprintf("Error: %v", err)
+		res.Write([]byte(response))
+		return
+	}
+	service = containerService
 }
 
 func authHandler(res http.ResponseWriter, req *http.Request) {
-     id := loadFileOrString(*clientID, *clientIDFile)
-     secret := loadFileOrString(*clientSecret, *clientSecretFile)
+	id := loadFileOrString(*clientID, *clientIDFile)
+	secret := loadFileOrString(*clientSecret, *clientSecretFile)
 
-     config, _ := example.NewClientConfigAndContext(id, secret)
+	config, _ := example.NewClientConfigAndContext(id, secret)
 
-     _, port, err := net.SplitHostPort(req.Host)
-     if err != nil {
-     	res.WriteHeader(500)
-	response := fmt.Sprintf("Error: %v", err)
-	res.Write([]byte(response))
-	return
+	_, port, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		res.WriteHeader(500)
+		response := fmt.Sprintf("Error: %v", err)
+		res.Write([]byte(response))
+		return
 	}
 
-     // This is the URL to return to
-     config.RedirectURL = fmt.Sprintf("http://localhost:%s/token", port)
+	// This is the URL to return to
+	config.RedirectURL = fmt.Sprintf("http://localhost:%s/token", port)
 
-     // This is the URL to send people to
-     redirectURL := example.SendTokenRequest(config)
-     http.Redirect(res, req, redirectURL, 302)
+	// This is the URL to send people to
+	redirectURL := example.SendTokenRequest(config)
+	http.Redirect(res, req, redirectURL, 302)
+}
+
+// Validates that there is a service impl. and that all keys in the params map are filled in from req
+func validateClientAndParameters(req *http.Request, params map[string]string) error {
+	if service == nil {
+		return errors.New("Service object is nil")
+	}
+	queryParams := req.URL.Query()
+	for key := range params {
+		value, ok := queryParams[key]
+		if !ok || len(value) == 0 || len(value[0]) == 0 {
+			return fmt.Errorf("Missing value for parameter: %s", key)
+		}
+		params[key] = value[0]
+	}
+	return nil
+}
+
+func randomPassword(size int) string {
+    const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    l := byte(len(charset))
+    data := make([]byte, size)
+    rand.Read(data)
+    for ix, val := range data {
+        data[ix] = charset[val % l]
+    }
+    return string(data)
+}
+
+func selectClusterHandler(res http.ResponseWriter, req *http.Request) {
+     params := map[string]string{"project": "", "zone": "", "cluster": ""}
+     	if err := validateClientAndParameters(req, params); err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to list: %v", err)))
+		return
+	}
+	cluster, err := service.Projects.Zones.Clusters.Get(params["project"], params["zone"], params["cluster"]).Do()
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to select: %v", err)))
+		return
+	}
+	data, err := json.Marshal(cluster)
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to select: %v", err)))
+		return
+	}
+	selectedCluster = cluster
+	res.WriteHeader(200)
+	res.Write(data)
+}
+
+func createClusterHandler(res http.ResponseWriter, req *http.Request) {
+	params := map[string]string{"project": "", "zone": "", "cluster": ""}
+	if err := validateClientAndParameters(req, params); err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to list: %v", err)))
+		return
+	}
+	cluster := &container.Cluster{
+		Name:             params["cluster"],
+		InitialNodeCount: 1,
+		MasterAuth: &container.MasterAuth{
+			    Username: "admin",
+			    Password: randomPassword(16),
+                },
+	}
+	createRequest := &container.CreateClusterRequest{cluster}
+	clusterResponse, err := service.Projects.Zones.Clusters.Create(params["project"], params["zone"], createRequest).Do()
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to create: %v", err)))
+		return
+	}
+	data, err := json.Marshal(clusterResponse)
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to create: %v", err)))
+		return
+	}
+	res.WriteHeader(200)
+	res.Write(data)
+}
+
+func listClusterHandler(res http.ResponseWriter, req *http.Request) {
+	params := map[string]string{"project": "", "zone": ""}
+	if err := validateClientAndParameters(req, params); err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to list: %v", err)))
+		return
+	}
+	list, err := service.Projects.Zones.Clusters.List(params["project"], params["zone"]).Do()
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to list: %v", err)))
+		return
+	}
+	data, err := json.Marshal(list)
+	if err != nil {
+		res.WriteHeader(500)
+		res.Write([]byte(fmt.Sprintf("Failed to list: %v", err)))
+		return
+	}
+	res.WriteHeader(200)
+	res.Write(data)
+}
+
+func selectDirector(req *http.Request) {
+     req.URL.Host = selectedCluster.Endpoint
+     req.URL.Scheme = "https"
+     req.SetBasicAuth(selectedCluster.MasterAuth.Username, selectedCluster.MasterAuth.Password)
 }
 
 func main() {
@@ -92,9 +218,16 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir(*dir)))
 	http.HandleFunc("/auth", authHandler)
 	http.HandleFunc("/token", tokenHandler)
+	http.HandleFunc("/list", listClusterHandler)
+	http.HandleFunc("/create", createClusterHandler)
+	http.HandleFunc("/select", selectClusterHandler)
+
+	proxy := &httputil.ReverseProxy{
+	      Director: selectDirector,
+	      Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+	}
+	http.Handle("/api/", proxy)
+
 	http.ListenAndServe(*addr, nil)
-
-
-
 
 }
--- www/script.js
+++ www/script.js
@@ -1,15 +1,53 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-    $scope.data = {
-	"selected": 0,
-        "clusters": [
-            {
+var StarterApp = function(http) {
+    this.http = http;
+    this.clusters = {
+        "create": {
 		"name": "Create a new GKE Cluster"
             },
-	    {
-		"name": "Manual Cluster"
-	    }
-	]
     };
+    this.zones = [
+	"asia-east1-a",
+	"asia-east1-b",
+	"asia-east1-c",
+	"europe-west1-b",
+	"europe-west1-c",
+	"europe-west1-d",
+	"us-central1-a",
+	"us-central1-b",
+        "us-central1-c",
+	"us-central1-f"
+    ];	
+};
+
+StarterApp.prototype.LoadClusters = function() {
+    return this.http.get('/list?project=' + this.project + '&zone=' + this.zone).
+	success(function(data, status) {
+	    angular.forEach(data.clusters, function(cluster) {
+		this.clusters[cluster.name] = {"name": cluster.name};
+	    }.bind(this));
+	}.bind(this)).
+	error(function(data, status) {
+	    console.log("Error!");
+	    console.log(status);
+	});
+};
+
+StarterApp.prototype.SelectCluster = function(cluster) {
+    return this.http.get('/select?project=' + this.project + '&zone=' + this.zone + '&cluster=' + cluster).
+	success(function(data, status) {
+	    console.log(data);
+	}).
+	error(function(data, status) {
+	    console.log("Error!");
+	    console.log(status);
+	});
+};
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http){
+    $scope.data = {
+	"selected": 0,
+    };
+    $scope.controller = new StarterApp($http);
 }]);
